a583e457832d0cfe230a77a984f68d3a2bd1f12e
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-04-08 09:50:06 +0300

    Add files via upload

---
+++ IntFigure/IntFigure/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntFigure
+{
+    class Rectangle : IFigure
+    {
+        public double Square(int a, int b)
+        {
+            return a * b;
+        }
+        public double Perimeter(int a, int b)
+        {
+            return (a + b) * 2;
+        }
+    }
+}
