da4b47cc9790a8606eba409ef852a60a3a532489
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-04-12 12:02:10 +0300

    Update Circle.cs

--- IntFigure/IntFigure/Circle.cs
+++ IntFigure/IntFigure/Circle.cs
@@ -8,5 +8,26 @@ namespace IntFigure
 {
     class Circle : IFigure
     {
+        public string Name { get; set; }
+        public int R { get; set; }
+        public Circle() { }
+        public Circle(string name, int r)
+        {
+            this.Name = name;
+            this.R = r;
+        }
+        public double Perimeter()
+        {
+            return 2 * Math.PI * R;
+        }
+        public double Square()
+        {
+            
+            return Math.PI * Math.Pow(R,2);
+        }
+        public override string ToString()
+        {
+            return $"Название геометрической фигуры: {Name}\nРадиус: {R}\nПериметр: {Perimeter():F2}\nПлощадь: {Square():F2}\n";
+        }
     }
 }
