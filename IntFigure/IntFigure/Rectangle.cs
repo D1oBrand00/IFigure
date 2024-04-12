e264ec277490b0e1f3255f4a302460e1afdb2392
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-04-12 12:00:57 +0300

    Update Rectangle.cs

--- IntFigure/IntFigure/Rectangle.cs
+++ IntFigure/IntFigure/Rectangle.cs
@@ -8,13 +8,27 @@ namespace IntFigure
 {
     class Rectangle : IFigure
     {
-        public double Square(int a, int b)
+        public string Name { get; set; }
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public Rectangle() { }
+        public Rectangle(string name, int side1, int side2)
         {
-            return a * b;
+            this.Name = name;
+            this.Side1 = side1;
+            this.Side2 = side2;
         }
-        public double Perimeter(int a, int b)
+        public double Perimeter()
         {
-            return (a + b) * 2;
+            return (Side1 + Side2) * 2;
+        }
+        public double Square()
+        {
+            return Side1 * Side2;
+        }
+        public override string ToString()
+        {
+            return $"Название геометрической фигуры: {Name}\nПервая сторона: {Side1}\nВторая сторона: {Side2}\nПериметр: {Perimeter():F2}\nПлощадь: {Square():F2}\n";
         }
     }
 }
