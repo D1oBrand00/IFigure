0e6c51a0bbefdb17a825f4e9cbf3abb61ca08cf6
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-04-12 12:00:02 +0300

    Update Program.cs

--- IntFigure/IntFigure/Program.cs
+++ IntFigure/IntFigure/Program.cs
@@ -10,6 +10,11 @@ namespace IntFigure
     {
         static void Main(string[] args)
         {
+            Rectangle rect1 = new Rectangle("Прямоугольник",3,5);
+            Triangle trng1 = new Triangle("Треугольник", 4, 6, 3);
+            Circle crc1 = new Circle("Окружность", 6);
+            Console.WriteLine($"{rect1.ToString()}\n{trng1.ToString()}\n{crc1.ToString()}");
+            Console.ReadKey();
         }
     }
 }
